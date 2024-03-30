9e9234e119e74f56921c87d2dd7da5d1b916870d
jljulioa <jljulioa@gmail.com>
2024-03-30 16:30:56 -0500

    CI/CD_Update_Docker

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,15 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: './',
+  preview: {
+    port: 8080,
+    strictPort: true
+  },
   server: {
-    host: true
+    port: 8080,
+    strictPort: true,
+    host: true,
+    origin: 'http://localhost:8080'
   }
 })
