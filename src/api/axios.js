0381df0377fbd337f199ab6400384f25fc0126d9
jljulioa <jljulioa@gmail.com>
2024-04-03 14:46:58 -0500

    CI/CD_Update

--- src/api/axios.js
+++ src/api/axios.js
@@ -2,13 +2,21 @@ import axios from "axios";
 import k8s from "@kubernetes/client-node";
 
 const createAxiosInstance = async () => {
-    const api = new k8s.CoreV1Api();
-    const service = await api.v1.namespaces('default').services('express-backend-service').get();
-    const axiosInstance = axios.create({
-        baseURL: `http://${service.spec.clusterIP}:${service.spec.ports[0].port}`,
-        withCredentials: true
-    });
-    return axiosInstance;
+    const kc = new k8s.KubeConfig();
+    kc.loadFromDefault();
+
+    const k8sApi = kc.makeApiClient(k8s.CoreV1Api);
+
+    try{
+        const service = await k8sApi.readNamespacedService('express-backend-service', 'default');
+        const axiosInstance = axios.create({
+            baseURL: `http://${service.spec.clusterIP}:${service.spec.ports[0].port}`,
+            withCredentials: true
+        });
+        return axiosInstance;
+    } catch (error) {
+        console.log(error);
+    } 
 }
 
 export default createAxiosInstance;
