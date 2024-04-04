d38eac93fe333ea7ce4b0920a4a77a928ce0f297
jljulioa <jljulioa@gmail.com>
2024-04-04 15:06:07 -0500

    CI/CD_UPdate_Axios

--- src/api/axios.js
+++ src/api/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 
 const axiosInstance = axios.create({
-    baseURL: `express-backend-service.default.svc.cluster.local:8080/api`,
+    baseURL: `http://express-backend-service.default.svc.cluster.local:8080/api`,
     withCredentials: true
 })
 
