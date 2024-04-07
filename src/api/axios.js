a724c6628c72c3bb6085738b850e74e94da6e984
jljulioa <jljulioa@gmail.com>
2024-04-07 15:19:14 -0500

    sks

--- src/api/axios.js
+++ src/api/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 
 const axiosInstance = axios.create({
-    baseURL: `http://express-backend-balancer-43295231.us-east-1.elb.amazonaws.com/api`,
+    baseURL: `http://internal-express-backend-balancer-1091790852.us-east-1.elb.amazonaws.com/api`,
     withCredentials: true
 })
 
